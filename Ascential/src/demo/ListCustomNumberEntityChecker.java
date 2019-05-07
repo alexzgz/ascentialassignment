5fdfea960b73437d3b5388b402db29af42a9040a
Alex Ortariz <alex.ortariz@gmail.com>
2019-05-07 16:52:02 +0200

    Performed

--- Ascential/src/demo/ListCustomNumberEntityChecker.java
+++ Ascential/src/demo/ListCustomNumberEntityChecker.java
@@ -2,8 +2,12 @@ package demo;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -19,6 +23,9 @@ public class ListCustomNumberEntityChecker implements NumberFinder{
 	private FastestComparator comparator;
 	
 	
+    // The fastest way is using a map. It was ideal for searching purposes
+	private Map<String, CustomNumberEntity> map;
+
 	public ListCustomNumberEntityChecker() {
 		comparator = new FastestComparator();
 	}
@@ -26,7 +33,7 @@ public class ListCustomNumberEntityChecker implements NumberFinder{
 	@Override
 	// Constraint 3 --> The contains method of your implementation MUST use   
 	// the provided FasterComparator.compare method to compare the int value with each  
-	// CustomNumberEntity. How you do thisin the fastest possible time is the key. 
+	// CustomNumberEntity. How you do this in the fastest possible time is the key. 
 	// FastestComparator.compare cannot be modified and no other comparison method should be 
 	// used (hashing, indexes etc)
 	// ***********************
@@ -34,14 +41,14 @@ public class ListCustomNumberEntityChecker implements NumberFinder{
 	// from FastestComparator will handle this. e.g. do not cast from int to String, or 
 	// CustomNumberEntity.number to int (even if is not used for comparison purpose)
 	public boolean contains(int valueToFind, List<CustomNumberEntity> list) {
+	    // The fastest way is using a map. It was ideal for searching purposes. We collect the map from a list
+        // Ignoring the duplicate keys
+		this.map = list.stream().collect(Collectors.toMap(a -> a.getNumber(), a -> a,   (a1, a2) -> {return a1;}));        
 		
-		for (CustomNumberEntity customNumberEntity : list) {
-
-			if (comparator.compare(valueToFind, customNumberEntity) == 0)
-				return true;
-		}
-		
-		return false;
+		if (map.containsKey(new String(valueToFind+"")))
+			return true;
+		else
+			return false;
 	}
 
 	
@@ -58,7 +65,8 @@ public class ListCustomNumberEntityChecker implements NumberFinder{
 		}        
 		
 		List<CustomNumberEntity> listCustomNumberEntity = new ArrayList<CustomNumberEntity>(arr.size());
-		
+        CustomNumberEntity customNumberEntity = null;
+
 		for (int i = 0; i < arr.size(); i++) {
 			JsonElement number = arr.get(i).getAsJsonObject().get("number");
 			String numberString = "";
@@ -66,13 +74,36 @@ public class ListCustomNumberEntityChecker implements NumberFinder{
 				numberString = number.getAsString();
 				if (ListCustomNumberEntityChecker.isNumeric(numberString))
 				{	
-					CustomNumberEntity customNumberEntity = listCustomNumberEntity.get(i);
+			        Constructor<CustomNumberEntity> constructor = null;
+					
+			        try {
+						constructor = CustomNumberEntity.class.getDeclaredConstructor();
+					} catch (NoSuchMethodException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (SecurityException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+			        
+			        constructor.setAccessible(true);
+					try {
+						customNumberEntity = constructor.newInstance();
+					} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+							| InvocationTargetException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
 					customNumberEntity.setNumber(numberString);
-					listCustomNumberEntity.set(i, customNumberEntity);
+					listCustomNumberEntity.add(customNumberEntity);
 				}	
-				
 			
         }
+		
+        // Ignoring the duplicate keys (we put it commented here because if needed in a future) --> It was ideal for searching purposes 
+		//this.map = listCustomNumberEntity.stream().collect(Collectors.toMap(a -> a.getNumber(), a -> a,   (a1, a2) -> {return a1;}));        
+
 		return listCustomNumberEntity;
 	}
 
--- Ascential/src/test/ListCustomNumberEntityCheckerTest.java
+++ Ascential/src/test/ListCustomNumberEntityCheckerTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
@@ -16,8 +17,19 @@ public class ListCustomNumberEntityCheckerTest {
 	public void testIsInList() {
 		ListCustomNumberEntityChecker checker = new ListCustomNumberEntityChecker();
 		List<CustomNumberEntity> list = checker.readFromFile("C:\\Users\\aortariz\\Desktop\\ej.json");
-		checker.contains(45, list);
         assertTrue(checker.contains(45, list));
 	}
 
+	
+	@Test
+	public void testIsNotInList() {
+		ListCustomNumberEntityChecker checker = new ListCustomNumberEntityChecker();
+		List<CustomNumberEntity> list = checker.readFromFile("C:\\Users\\aortariz\\Desktop\\ej.json");
+        assertFalse(checker.contains(495, list));
+	}
+
+	// We tried to run into the array list, and it lasted 37 seconds
+	// We tried to use the map, and it lasted 0,001 seconds
+	
+	
 }
